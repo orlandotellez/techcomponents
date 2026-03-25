3cf40dc133d989ed737fa7a895763a869ee349b3
orlandotellez <espadachinkinno@gmail.com>
2026-03-24 23:05:25 -0600

    feat: agregar transaccion para crear un account a la par del usuario, si el account no se crear el usuario tampoco

--- backend/src/features/auth/register/service.rs
+++ backend/src/features/auth/register/service.rs
@@ -15,6 +15,11 @@ impl RegisterService {
     pub async fn register_user(db: &DbState, payload: RegisterRequest) -> Result<User, AppError> {
         let hashed_password: String = hash_password(&payload.password)?;
 
+        // iniciamos una transaccion para poder crear el usuario solo si se crea el account(si el
+        // account falla no se crea el usuario)
+        let mut tx = db.begin().await?;
+
+        // crear el usuario
         let user: User = sqlx::query_as!(
             User,
             r#"
@@ -47,6 +52,34 @@ impl RegisterService {
         )
         .fetch_one(db)
         .await?;
+
+        // crear el account(si falla no se crea el usuario)
+        sqlx::query!(
+            r#"
+            INSERT INTO account (
+                id,
+                account_id,
+                provider_id,
+                user_id,
+                password,
+                created_at,
+                updated_at
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            "#,
+            Uuid::new_v4(),
+            user.email,
+            "credentials", // por default será "credentials", más adelante sepuede poner otro provider_id como "google", "github"... etc
+            user.id,
+            hashed_password,
+            Utc::now(),
+            Utc::now()
+        )
+        .execute(&mut *tx)
+        .await?;
+
+        // confirmar transaccion(crear usuario y account)
+        tx.commit().await?;
 
         Ok(user)
     }
